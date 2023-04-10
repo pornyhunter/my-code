44e5f55fa0c1533f3693ea32fdff2486a296ca5b
pornyhunter <101872035+pornyhunter@users.noreply.github.com>
2023-04-10 08:40:08 +0530

    New upation
    program updated

--- hello.py
+++ hello.py
@@ -1,35 +1,7 @@
-# PYTHON LISTS:   (LISTS are mutable  --- can be edited once created on the other hand tuples are immutable)
 
-cars = ["audi", "GTR" , "porche" , "ferrari" , "dodge" , "benz"]
 
-me = cars[0]
+ips = ['100.122.133.105', '100.122.133.111']
 
-print(me)
+user_in = int(input("Enter the index number : "))
 
-#length of the list printed :
-print(len(cars))
-
-# inserting to a list;
-
-cars.insert(2,"Jaguar")
-
-print(cars)
-
-#apending to a list: --> will be done at the last:
-
-cars.append("JEEP")
-print(cars)
-
-# concatinating two list:
-
-colour_list = ["blue" , "pink" , "red" ,"yellow" ,"orange"]
-
-x = colour_list + cars
-
-print(x)
-
-# deleting from the end of a LIST;
-
-cars.pop(-1)
-
-print(cars)
+print(f"you choose {ips[user_in]}")
